b4b7cdfe949026a851bddb699f3d7f56b421f783
Nathan Francis <nathan@Nathans-MacBook-Pro.local>
2025-06-13 16:03:12 +0300

    Invetory UI done

---
+++ lib/ui/tables/inventory/inventory_details_table/inventory_details_data_table.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:shadcn_flutter/shadcn_flutter.dart' as shadcn;
+
+import '../../../../util/widget_constants.dart';
+
+class InvoiceDetailsDataTable extends StatelessWidget {
+  const InvoiceDetailsDataTable({super.key, required this.selectedInvoice});
+
+  final List<Map<String, dynamic>> selectedInvoice;
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = shadcn.Theme.of(context).colorScheme;
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: DataTable(
+        showCheckboxColumn: false ,
+        dividerThickness: 0.2,
+        headingTextStyle: kDefaultFont(context),
+        dataTextStyle: kDefaultFont(context),
+        columns: selectedInvoice.first.keys.map((key) {
+          return DataColumn(
+            label: Text(key),
+            numeric: key == 'numeric',
+          );
+        }).toList(),
+        rows: selectedInvoice.asMap().entries.map((entry) {
+          int index = entry.key;
+          Map<String, dynamic> invoice = entry.value;
+
+          return DataRow(
+            onSelectChanged: (value){
+
+            },
+            color: WidgetStateProperty.resolveWith<Color?>(
+                  (Set<WidgetState> states) {
+                return index % 2 == 0
+                    ? states.contains(WidgetState.hovered)
+                    ? colorScheme.destructive
+                    : colorScheme.background.withOpacity(0.8) // Even row color
+                    : states.contains(WidgetState.hovered)
+                    ? colorScheme.destructive
+                    : colorScheme.secondary.withOpacity(0.8); // Odd row color
+              },
+            ),
+            cells: invoice.entries.map((cell) {
+              return DataCell(Text(cell.value.toString()));
+            }).toList(),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
